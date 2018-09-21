2fe837fd3e144f6a23c01505c5e4e73d79794424
Abhishekkumar Israni <aiv700@udml7.cs.uregina.ca>
2018-09-21 12:51:03 -0600

    Connect the UI to code part is implemented

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITextFieldDelegate {
+    
+    //MARK:Properties
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    // Handle the text field's user input through delegate callback
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        //here self is refering to viewController class because its scope is inside within the class
+        nameTextField.delegate = self
+        
+        }
+   
+        //MARK:UITextFieldDelegate
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //It will close the keyboard to remove its first responder status
+        textField.resignFirstResponder()
+        
+        // return true means user will respond by pressing the return key after typing the text
+        return true
+        }
+    
+        //MARK: after the textFieldShouldReturn funct we need the below function because after resigning the first responder status the system calls this method
+        func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+       }
+    
 
-    override func didReceiveMemoryWarning() {
+       override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+        mealNameLabel.text="Default Text"
+        
+    }
 }
 
