e2ebb0c5a89265db2e3f26f4ea02cf23bbd09e22
Administrator <aiv700@udml15.local>
2018-09-26 21:46:49 -0600

    AppComplete Congratulations

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -9,25 +9,68 @@
 
 //  UIKit also gives you access to Foundation, we can remove the redundant import to Foundation.
 import UIKit
-
-class Meal {
+import os.log
+class Meal:NSObject , NSCoding {
+ 
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
     
+    
+    
+//    encode(with aCoder: NSCoder)
+    required convenience init?(coder aDecoder: NSCoder) {
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        // Because photo is an optional property of Meal, just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
+
     //MARK: Properties
     
     var name: String
     var photo: UIImage?
     var rating: Int
- 
+    
+    //MARK: Archiving Paths
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     
     // Initializer is a method that prepares an instance of a class for use, which involves setting an initial value for each property and performing any other setup or initialization.
     //MARK: Initialization
     init?(name: String, photo: UIImage?, rating: Int) {
         
         
-        // Initialization should fail if there is no name or if the rating is negative.
-        if name.isEmpty || rating < 0  {
+        // The name must not be empty
+        guard !name.isEmpty else {
             return nil
         }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+
+        
         // Initialize stored properties.
         self.name = name
         self.photo = photo
--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -35,6 +35,14 @@ class MealViewController: UIViewController , UITextFieldDelegate, UIImagePickerC
         //here self is refering to viewController class because its scope is inside within the class
         nameTextField.delegate = self
         
+        // Set up views if editing an existing Meal. here the meal property will only be non nil if the existed meal is updated
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text   = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         // Enable the Save button only if the text field has a valid Meal name.
         updateSaveButtonState()
         
@@ -94,8 +102,22 @@ class MealViewController: UIViewController , UITextFieldDelegate, UIImagePickerC
     //MARK: Navigation
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
+       
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+       
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+            
         
-        dismiss(animated: true, completion: nil)
+        else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        }
+        
+        else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
     
     // This method lets you configure a view controller before it's presented.
